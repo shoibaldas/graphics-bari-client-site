35b9690e7fe055067764086d2ce3940ba191c2f1
shoibaldas <shoibalshantu@gmail.com>
2022-11-09 22:10:18 +0600

    Feedback from client side on sepecific service is completed

---
+++ src/pages/Reviews/AddReviews/AddReviews.js
@@ -0,0 +1,96 @@
+import React, { useContext, useState } from 'react';
+import { FaStar } from "react-icons/fa";
+import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider';
+
+const colors = {
+    orange: "#FFBA5A",
+    grey: "#a9a9a9"
+
+};
+
+const AddReviews = () => {
+    const { user } = useContext(AuthContext);
+    const { _id, title } = useLoaderData();
+    const [currentValue, setCurrentValue] = useState(0);
+    const [hoverValue, setHoverValue] = useState(undefined);
+    const stars = Array(5).fill(0)
+
+    const handleClick = value => {
+        setCurrentValue(value)
+    }
+
+    const handleMouseOver = newHoverValue => {
+        setHoverValue(newHoverValue)
+    };
+
+    const handleMouseLeave = () => {
+        setHoverValue(undefined)
+    }
+
+    const handleReview = (event) => {
+        event.preventDefault();
+        const message = event.target.message.value;
+        const email = user?.email || 'unregistered';
+        const userId = user?.uid;
+        
+        const addReview = {
+
+            serviceId: _id,
+            serviceName: title,
+            userId,
+            email,
+            message,
+
+        }
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(addReview)
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => (error.message));
+    }
+
+    return (
+        <div className='p-6'>
+            <div className="flex flex-col max-w-xl p-8 border-2 rounded lg:p-10 text-gray-800">
+                <form onSubmit={handleReview} className="flex flex-col items-center w-full">
+                    <h2 className="text-3xl font-semibold text-center">Your opinion matters!</h2>
+                    <div className="flex flex-col items-center py-6 space-y-3">
+                        <span className="text-center">How was your experience?</span>
+                        <div className="flex space-x-3">
+                            {stars.map((_, index) => {
+                                return (
+                                    <FaStar
+                                        key={index}
+                                        size={24}
+                                        onClick={() => handleClick(index + 1)}
+                                        onMouseOver={() => handleMouseOver(index + 1)}
+                                        onMouseLeave={handleMouseLeave}
+                                        color={(hoverValue || currentValue) > index ? colors.orange : colors.grey}
+                                        style={{
+                                            marginRight: 10,
+                                            cursor: "pointer"
+                                        }}
+                                    ></FaStar>
+                                )
+                            })}
+                        </div>
+                    </div>
+                    <div className="flex flex-col w-full">
+                        <textarea className='input input-bordered p-3 h-32' name='message' placeholder='Your Feedback' required></textarea>
+                        <div className="form-control mt-4">
+                            <button type="submit" className="bg-sky-700 text-gray-50 font-semibold hover:bg-sky-800 py-3 rounded-md">Add Review</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default AddReviews;
--- src/pages/Services/ServiceDetails.js
+++ src/pages/Services/ServiceDetails.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { TbCurrencyTaka } from 'react-icons/tb';
 import { useLoaderData } from 'react-router-dom';
+import AddReviews from '../Reviews/AddReviews/AddReviews';
+
 
 const ServiceDetails = () => {
     const service = useLoaderData();
     return (
-        <div className='flex mt-8 md:mt-0 justify-center'>
-            <div className="w-10/12 z-0 p-4 rounded-md shadow-md">
-                <div className="flex justify-between pb-4 border-bottom">
-                    <div className="flex items-center">
-                        <h2 className="mb-0 capitalize text-3xl font-semibold dark:text-sky-700">{service.title}</h2>
-                    </div>
+        <div className=''>
+            <div className="max-w-screen-lg mx-auto p-4 rounded-md shadow-md">
+                <div className="pb-4">
+                    <h2 className="mb-0 capitalize text-3xl font-semibold dark:text-sky-700">{service.title}</h2>
                 </div>
-
                 <div className="space-y-1">
                     <div className="space-y-2">
                         <img src={service.img} alt="" className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500" />
-                        <div className="flex p-2 border-t-2 border-sky-700">
+                        <div className="p-2 border-t-2 border-sky-700">
                             <div className="flex items-center space-x-1">
                                 <TbCurrencyTaka className='text-green-500'></TbCurrencyTaka>
                                 <span className='text-orange-600'>{service.price}</span>
@@ -28,11 +27,15 @@ const ServiceDetails = () => {
                             <h1 className="text-xl text-start font-semibold text-sky-700">Description :</h1>
                             <p className="leading-snug text-start text-gray-700">{service.description}</p>
                         </div>
-                        
                     </div>
                 </div>
+                <h3 className='text-3xl font-semibold mt-12'>Reviews</h3>
+                <div className='border-t-2 border-gray-400'></div>
+                <AddReviews></AddReviews>
             </div>
-        </div >
+
+        </div>
+
     );
 };
 
